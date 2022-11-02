520ef8e0f22ff282e01878abca1b0751fc6fc4cc
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-11-02 14:51:02 +0200

    dislike button not ready yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,68 @@
-import './App.css';
-import './Card.css';
-import React, { Component } from 'react';
-import './Header.css';
-import Header from './Header';
-import Card from './Card';
-import { animals } from './animals';
+import "./App.css";
+import "./Card.css";
+import React, { Component } from "react";
 
+import Card from "./Card";
+import { animals } from "./animals";
 
-class App extends Component{
-
+class App extends Component {
   state = {
     animals: animals,
+    search: "",
   };
-removeHandler = (name) => {
-  console.log("remove was clicked", name);
-}
-  addHandler = () => {
-   console.log('like was clicked')
-  }
-  animalsList = this.state.animals.map((animal) => {
-    return <Card key={animal.name} 
-    name={animal.name} 
-    likes={animal.likes} 
-    removeCard={() => this.removeHandler(animal.name)} 
-    addLikes={this.addHandler} />;
-});
+  removeHandler = (name) => {
+    const updatedArray = this.state.animals.filter(
+      (animal) => animal.name !== name
+    );
+
+    this.setState({ animals: updatedArray });
+  };
+
+  searchHandler = (e) => {
+    this.setState({ search: e.target.value });
+    console.log(e.target.value);
+  };
+
+  addHandler = (name) => {
+    this.setState((state) => {
+      const updatedArray = state.animals.map((animal) => {
+        if (animal.name === name) {
+          return { ...animal, likes: animal.likes + 1 };
+        } else {
+          return animal;
+        }
+      });
+      return { animals: updatedArray };
+    });
+  };
 
   render() {
-  
-  return ( <div>
+    const animalFilter = this.state.animals.filter((animal) => {
+      return animal.name.includes(this.state.search);
+    });
 
-    <Header/>
-   {this.animalsList}
-  </div>
-  )
-};
-};
+    const animalsList = animalFilter.map((animal) => {
+      return (
+        <Card
+          key={animal.name}
+          name={animal.name}
+          likes={animal.likes}
+          removeCard={() => this.removeHandler(animal.name)}
+          addLikes={() => this.addHandler(animal.name)}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <div className="header">
+        <h1>{animalsList.length} Animals List</h1>
+        <input className="search" type="text" onChange={this.searchHandler} />
+        </div>
+        <div className="animal">{animalsList}</div>
+      </div>
+    );
+  }
+}
 
 export default App;
-
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import './Card.css';
+import "./Card.css";
 import { FaHeart } from "react-icons/fa";
 
-const Card=(props)=> {
-    
-    return (
-    
+const Card = (props) => {
+  return (
     <div className="card">
       <div className="img-wrap">
-        <span class="close" onClick={props.removeCard} >&times;</span>
-        <img src={`https://source.unsplash.com/500x400/?${props.name}`} alt={props.name}></img>
+        <span className="close" onClick={props.removeCard}>
+          &times;
+        </span>
+        <img
+          src={`https://source.unsplash.com/400x400/?${props.name}`}
+          alt={props.name}
+        ></img>
       </div>
-        
-        <div className="container">
-            <h2>{props.name} </h2>
-            <p><FaHeart/> {props.likes}</p>
-            <button onClick={props.addLikes}>Add likes</button>
-         </div>
-            
+
+      <div className="container">
+        <h2>{props.name} </h2>
+        <div className="likes">
+        <button className="button-85" onClick={props.addLikes}>Dislikes</button>
+          <span><FaHeart /><p>{props.likes}</p> </span>
+          <button className="button-85" onClick={props.addLikes}>Add likes</button>
+        </div>
+      </div>
     </div>
-   
-
-    );
-    
+  );
 };
-
-
 
 export default Card;
